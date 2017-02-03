d24c032a2fbc45c255bcd3f6f1c01c9c15ccbc75
Unknown <christopher.nyberg@gmail.com>
2017-02-03 14:13:19 -0800

    Following along
    Making good progress and everything is making sense thus far.

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,35 @@ module Lib
     ( someFunc
     ) where
 
+type WordList = [String]
+
+
+minWordLength :: Int 
+minWordLength = 6
+
+
+maxWordLength :: Int 
+maxWordLength = 9
+
+
+allWords :: IO WordList
+allWords = do
+    dict <- readFile "data/dict.txt" 
+    return (lines dict)
+
+
+gameLength :: String -> Bool
+gameLength w =
+    let l = length (w :: String)
+    in l > minWordLength && l < maxWordLength
+
+
+gameWords :: IO WordList
+gameWords = do
+    aw <- allWords
+    return (filter gameLength aw)
+
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc =
+    gameWords >>= print
