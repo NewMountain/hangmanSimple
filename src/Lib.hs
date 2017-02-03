e30a56cb03210d7eff56668400a89689497b06b7
Unknown <christopher.nyberg@gmail.com>
2017-02-03 14:25:55 -0800

    Updated file
    Latest working version. Now reads all words and prints a random work selected from the list.

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,35 +2,38 @@ module Lib
     ( someFunc
     ) where
 
-type WordList = [String]
+import System.Random (randomRIO)
 
+type WordList = [String]
 
 minWordLength :: Int 
 minWordLength = 6
 
-
 maxWordLength :: Int 
 maxWordLength = 9
-
 
 allWords :: IO WordList
 allWords = do
     dict <- readFile "data/dict.txt" 
     return (lines dict)
 
-
 gameLength :: String -> Bool
 gameLength w =
     let l = length (w :: String)
-    in l > minWordLength && l < maxWordLength
-
+    in  l > minWordLength && l < maxWordLength
 
 gameWords :: IO WordList
 gameWords = do
     aw <- allWords
     return (filter gameLength aw)
 
+randomWords :: WordList -> IO String
+randomWords wl = do
+    randomIndex <- randomRIO(0, maxIndex)
+    return $ wl !! randomIndex
+    where 
+        maxIndex = length wl - 1
 
 someFunc :: IO ()
 someFunc =
-    gameWords >>= print
+    gameWords >>= randomWords >>= print
